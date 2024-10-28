fc9550745e7b9860efa3c0bc20a53b99e454c02b
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2024-10-28 10:23:28 +0100

    Camera translation x/y

--- Maker/Editor/main.cpp
+++ Maker/Editor/main.cpp
@@ -22,7 +22,6 @@ using namespace std;
 #include "Engine/MyWindow.h"
 #include <glm/gtc/type_ptr.hpp>
 #include <SDL2/SDL_events.h>
-using namespace std;
 
 using hrclock = chrono::high_resolution_clock;
 using u8vec4 = glm::u8vec4;
@@ -32,17 +31,14 @@ using vec3 = glm::dvec3;
 static const ivec2 WINDOW_SIZE(1280, 720);
 static const unsigned int FPS = 60;
 static const auto FRAME_DT = 1.0s / FPS;
-using namespace std;
 
 static Camera camera;
-
 static GameObject go;
 
-
-
+static bool middleMousePressed = false;
+static ivec2 lastMousePosition;
 
 static void drawFloorGrid(int size, double step) {
-	//glColor3ub(0, 0, 0);
 	glBegin(GL_LINES);
 	for (double i = -size; i <= size; i += step) {
 		glVertex3d(i, 0, -size);
@@ -51,11 +47,9 @@ static void drawFloorGrid(int size, double step) {
 		glVertex3d(size, 0, i);
 	}
 	glEnd();
-
 }
 
-void configureCamera()
-{
+void configureCamera() {
 	glm::dmat4 projectionMatrix = camera.projection();
 	glm::dmat4 viewMatrix = camera.view();
 
@@ -68,13 +62,9 @@ void configureCamera()
 
 static void display_func() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 	configureCamera();
-
 	drawFloorGrid(16, 0.25);
-
 	go.draw();
-
 }
 
 static void init_opengl() {
@@ -96,40 +86,44 @@ static void mouseWheel_func(int wheel, int direction, int x, int y) {
 	camera.transform().translate(vec3(0, 0, direction * 0.1));
 }
 
-const char* checkFile(char* file)
-{
+static void mouseButton_func(int button, int state, int x, int y) {
+	if (button == SDL_BUTTON_MIDDLE) {
+		middleMousePressed = (state == SDL_PRESSED);
+		if (middleMousePressed) {
+			lastMousePosition = ivec2(x, y); // Guardar la posición inicial del ratón
+		}
+	}
+}
+
+static void mouseMotion_func(int x, int y) {
+	if (middleMousePressed) {
+		// Calcular el movimiento basado en la diferencia de la posición del ratón
+		ivec2 delta = ivec2(x, y) - lastMousePosition;
+		lastMousePosition = ivec2(x, y);
+
+		// Mover la cámara basado en el movimiento del ratón
+		camera.transform().translate(vec3(delta.x * 0.01, 0, -delta.y * 0.01));
+	}
+}
+
+const char* checkFile(char* file) {
 	string filePath = file;
 	string extension;
-
 	extension = filePath[filePath.size() - 3] + filePath[filePath.size() - 2] + filePath[filePath.size() - 1];
-
 	const char* ext = extension.data();
-
 	return ext;
-	
 }
 
 int main(int argc, char* argv[]) {
-	// Iniit window and context
 	ilInit();
 	iluInit();
 	ilutInit();
 	MyWindow window("SDL2 Simple Example", WINDOW_SIZE.x, WINDOW_SIZE.y);
-
 	init_opengl();
 
-	// Init camera
 	camera.transform().pos() = vec3(0, 1, 4);
 	camera.transform().rotate(glm::radians(180.0), vec3(0, 1, 0));
 
-	// Debugger for the Model Loader
-	struct aiLogStream stream;
-	stream = aiGetPredefinedLogStream(aiDefaultLogStream_DEBUGGER, nullptr);
-	aiAttachLogStream(&stream);
-
-	
-	
-	
 	SDL_Event event;
 	char* dropped_filePath;
 	auto mesh = make_shared<Mesh>();
@@ -143,32 +137,26 @@ int main(int argc, char* argv[]) {
 		const auto dt = t1 - t0;
 		if (dt < FRAME_DT) this_thread::sleep_for(FRAME_DT - dt);
 
-		while (SDL_PollEvent(&event))
-		{
-			switch (event.type)
-			{
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
 			case SDL_DROPFILE:
 				dropped_filePath = event.drop.file;
 				mesh->LoadFile(dropped_filePath);
 				imageTexture->LoadTexture("Baker_house.png");
-				
 				go.setMesh(mesh);
 				go.setTextureImage(imageTexture);
-				
-				
-				//go.setTextureImage(imageTexture);
 				SDL_free(dropped_filePath);
-				
-				
+				break;
+			case SDL_MOUSEBUTTONDOWN:
+			case SDL_MOUSEBUTTONUP:
+				mouseButton_func(event.button.button, event.button.state, event.button.x, event.button.y);
+				break;
+			case SDL_MOUSEMOTION:
+				mouseMotion_func(event.motion.x, event.motion.y);
 				break;
 			}
 		}
-
 	}
-	
-
-	
-
 
 	return EXIT_SUCCESS;
 }
