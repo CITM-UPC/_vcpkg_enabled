c31842fdecd78d92dc825dabdd92ca5d8b6f8194
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2024-10-30 13:09:54 +0100

    cambios en el transform

--- Maker/Engine/Transform.cpp
+++ Maker/Engine/Transform.cpp
@@ -11,6 +11,20 @@ void Transform::rotate(double rads, const vec3& v) {
 	_mat = glm::rotate(_mat, rads, v);
 }
 
+void Transform::alignCamera(const vec3& worldUp) {
+
+    vec3 fwd = glm::normalize(_fwd);
+    vec3 right = glm::normalize(glm::cross(worldUp, fwd));
+    vec3 up = glm::cross(fwd, right);
+
+
+    _left = right;
+    _up = up;
+    _fwd = fwd;
+    _pos = _pos;
+    _mat = mat4(vec4(_left, 0.0f), vec4(_up, 0.0f), vec4(_fwd, 0.0f), vec4(_pos, 1.0f));
+}
+
 void Transform::lookAt(const vec3& target) {
     _fwd = glm::normalize(_pos - target);
     _left = glm::normalize(glm::cross(vec3(0, 1, 0), _fwd));
