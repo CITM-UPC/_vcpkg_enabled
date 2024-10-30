f87f2a1b96deb43b6918af5be9a87e4efc8eb3fe
Noelswag <125265329+Noelswag@users.noreply.github.com>
2024-10-30 13:52:37 +0100

    Ensures gameobjects have different names

--- Maker/Editor/MyGUI.cpp
+++ Maker/Editor/MyGUI.cpp
@@ -9,35 +9,31 @@
 #include <tinyfiledialogs/tinyfiledialogs.h> 
 
 MyGUI::MyGUI(SDL_Window* window, void* context) {
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-	ImGui::StyleColorsDark();
-	ImGui_ImplSDL2_InitForOpenGL(window, context);
-	ImGui_ImplOpenGL3_Init();
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    ImGui::StyleColorsDark();
+    ImGui_ImplSDL2_InitForOpenGL(window, context);
+    ImGui_ImplOpenGL3_Init();
 }
 
 MyGUI::~MyGUI() {
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplSDL2_Shutdown();
-	ImGui::DestroyContext();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
 }
-
-
 
 FileManager fileManager;
 GameObject* selectedGameObject = nullptr; // Define selectedGameObject
-
-// MyGUI.cpp
 
 void MyGUI::render() {
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplSDL2_NewFrame();
     ImGui::NewFrame();
 
-	//Main Menu bar, Comment the line below if you don't want the main menu bar to appear
+    //Main Menu bar, Comment the line below if you don't want the main menu bar to appear
     if (ImGui::BeginMainMenuBar()) {
         if (ImGui::BeginMenu("File")) {
             if (ImGui::MenuItem("Import FBX")) {
@@ -52,35 +48,32 @@ void MyGUI::render() {
                     0
                 );
                 if (filePath) {
-					GameObject go;
+                    GameObject go;
                     fileManager.LoadFile(filePath, go);
-					scene.emplaceChild(go);
-                    // Handle the selected file path
-                    // For example, you can load the FBX file here
-                    // loadFBX(filePath);
+                    scene.emplaceChild(go);
                 }
             }
             if (ImGui::MenuItem("Quit")) {
-				SDL_Quit();
+                SDL_Quit();
             }
             ImGui::EndMenu(); // Close the "File" menu
 
         }
-		if (ImGui::BeginMenu("Help")) {
+        if (ImGui::BeginMenu("Help")) {
             if (ImGui::MenuItem("About")) {
                 // Open a webpage when "About" is clicked
                 const char* url = "https://github.com/CITM-UPC/Badass-Engine";
                 SDL_OpenURL(url);
             }
-			
-			ImGui::EndMenu(); // Close the "Edit" menu
-		}
+
+            ImGui::EndMenu(); // Close the "Edit" menu
+        }
         ImGui::EndMainMenuBar(); // Close the main menu bar
     }
 
     ImGui::SetNextWindowSize(ImVec2(300, 700), ImGuiCond_Always);
     ImGui::SetNextWindowPos(ImVec2(0, 20), ImGuiCond_Always);
-	//GameObject window, Comment the line below if you don't want the GameObject window to appear
+    //GameObject window, Comment the line below if you don't want the GameObject window to appear
     if (ImGui::Begin("Gameobjects", NULL, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove)) {
         for (auto& child : scene.getChildren()) {
             static char newName[128] = "";
@@ -113,7 +106,7 @@ void MyGUI::render() {
 
     ImGui::SetNextWindowSize(ImVec2(500, 700), ImGuiCond_Always);
     ImGui::SetNextWindowPos(ImVec2(780, 20), ImGuiCond_Always);
-	//Inspector window, Comment the line below if you don't want the Inspector window to appear
+    //Inspector window, Comment the line below if you don't want the Inspector window to appear
     if (ImGui::Begin("Inspector", NULL, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove)) {
         if (selectedGameObject) {
             ImGui::Text("Selected GameObject: %s", selectedGameObject->name.c_str());
@@ -125,7 +118,7 @@ void MyGUI::render() {
                       static_cast<float>(selectedGameObject->_transform->GetPosition().z) };
 
             if (ImGui::InputFloat3("##position", position)) {
-                selectedGameObject->_transform->GetPosition() = glm::vec3(position[0], position[1], position[2]);
+                //selectedGameObject->_transform->SetPosition(glm::vec3(position[0], position[1], position[2]));
             }
 
             // Display rotation
@@ -136,35 +129,39 @@ void MyGUI::render() {
             if (ImGui::InputFloat3("##rotation", rotationArray)) {
                 //selectedGameObject->_transform->SetRotation(glm::vec3(rotationArray[0], rotationArray[1], rotationArray[2]));
             }
-            
+
             if (ImGui::Checkbox("Draw Texture", &selectedGameObject->GetComponent<MeshLoader>()->drawTexture)) {
                 if (selectedGameObject->GetComponent<MeshLoader>()->drawTexture)
                 {
                     selectedGameObject->GetComponent<MeshLoader>()->GetMesh()->deleteCheckerTexture();
                 }
             }
-                }
-                
+        }
         else {
             ImGui::Text("No GameObject selected.");
         }
     }
     ImGui::End();
+    // Ensure no two GameObjects have the same name
+    std::unordered_set<std::string> nameSet;
+    for (auto& child : scene.getChildren()) {
+        if (nameSet.find(child.name) != nameSet.end()) {
+            // If a duplicate name is found, append a unique suffix
+            int suffix = 1;
+            std::string newName = child.name + "_" + std::to_string(suffix);
+            while (nameSet.find(newName) != nameSet.end()) {
+                suffix++;
+                newName = child.name + "_" + std::to_string(suffix);
+            }
+            child.name = newName;
+        }
+        nameSet.insert(child.name);
+    }
     //ImGui::ShowDemoWindow();
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
-
-
-
-
-
-
-
-
-
-
 void MyGUI::processEvent(const SDL_Event& event) {
-	ImGui_ImplSDL2_ProcessEvent(&event);
+    ImGui_ImplSDL2_ProcessEvent(&event);
 }
